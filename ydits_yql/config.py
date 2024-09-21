f928a9a6a65fb36c00a893042ea2d77fa0e0422e
よね/Yone <git@yoneyo.com>
2024-09-21 14:34:32 +0900

    Confidential

--- ydits_yql/config.py
+++ ydits_yql/config.py
@@ -1,6 +1,6 @@
 DISCORD_BOT = {
-    "token": "MTA5NDkzMDY1NzMzNzE2Nzg3Mw.GIjayp.TrS_GDydsmDGu2za4rk1zkn6bpIqcLydDVs6Tk ",
+    "token": "",
     "channels": {
-        "vxww50": 1118704733562097746,
+        "vxww50": 0000000000000000000,
     },
 }

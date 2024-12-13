6204eaa6092f627b5ae739cb87c2c958c446fee4
よね/Yone <git@yoneyo.com>
2024-12-13 23:46:09 +0900

    refactor(__main__.py): インポート参照の変更

--- ydits_yql/__main__.py
+++ ydits_yql/__main__.py
@@ -1,5 +1,7 @@
 import logging
 import ydits_yql
+from ydits_yql import config
+from ydits_yql.bot import Bot
 from ydits_yql.lib.clear_console import clear_console
 
 
@@ -9,11 +11,13 @@ if __name__ == "__main__":
     print(f"{ydits_yql.__title__} を起動しています...")
 
     log_handler = logging.FileHandler(
-        filename="discord.log", encoding="utf-8", mode="w"
+        filename="discord.log",
+        encoding="utf-8",
+        mode="w",
     )
 
-    my_bot = ydits_yql.Bot(
-        token=ydits_yql.config.DISCORD_BOT["token"],
+    my_bot = Bot(
+        token=config.DISCORD_BOT["token"],
         log_handler=log_handler,
-        channels_id=ydits_yql.config.DISCORD_BOT["channels"],
+        channels_id=config.DISCORD_BOT["channels"],
     )
